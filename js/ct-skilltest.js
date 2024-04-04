61958a3e01e7e2471bc24eea5da7e8da6f03e522
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2024-04-03 21:19:56 -0600

    nav js

--- js/ct-skilltest.js
+++ js/ct-skilltest.js
@@ -1,12 +1,13 @@
+// pane function
 function activateTab(tab) {
     var tabs = document.querySelectorAll('.content-item');
     var tabsPane = document.querySelectorAll('.tab-pane');
 
-    tabs.forEach(function(tab) {
+    tabs.forEach(function (tab) {
         tab.classList.remove('active-tab');
     });
 
-    tabsPane.forEach(function(pane) {
+    tabsPane.forEach(function (pane) {
         pane.classList.remove('active');
     });
 
@@ -16,3 +17,36 @@ function activateTab(tab) {
     var activePane = document.getElementById(tabId);
     activePane.classList.add('active');
 }
+
+// nav function
+document.addEventListener("DOMContentLoaded", function () {
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function (navLink) {
+        var dropdownMenu = navLink.querySelector('.dropdown-menu');
+        var subDropMenus = navLink.querySelectorAll('.sub-drop-menu');
+        var anchorTag = navLink.querySelector('a');
+
+        navLink.addEventListener('mouseenter', function () {
+            dropdownMenu.classList.add('show');
+            anchorTag.classList.add('link-active');
+        });
+
+        navLink.addEventListener('mouseleave', function () {
+            dropdownMenu.classList.remove('show');
+            anchorTag.classList.remove('link-active');
+        });
+
+        subDropMenus.forEach(function (subDropMenu) {
+            subDropMenu.parentNode.addEventListener('mouseenter', function () {
+                subDropMenu.classList.add('show');
+            });
+
+            subDropMenu.parentNode.addEventListener('mouseleave', function () {
+                subDropMenu.classList.remove('show');
+            });
+        });
+    });
+});
+
+
